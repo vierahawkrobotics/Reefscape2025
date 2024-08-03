d4f96e839d86a042cc803bb3deea102691303d19
stamparkour <42157566+stamparkour@users.noreply.github.com>
2024-08-03 15:04:13 -0400

    formating

--- src/main/java/frc/robot/Testing/TestingState.java
+++ src/main/java/frc/robot/Testing/TestingState.java
@@ -2,7 +2,13 @@ package frc.robot.testing;
 
 
 public class TestingState {
-    public static void initialize() {}
-    public static void periodic() {}
-    public static void exit() {}
+    public static void initialize() {
+
+    }
+    public static void periodic() {
+
+    }
+    public static void exit() {
+        
+    }
 }
