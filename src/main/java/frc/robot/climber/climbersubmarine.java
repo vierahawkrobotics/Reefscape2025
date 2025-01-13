97e0201b8f3949f84b2d8cc3fb1a1d3d36e2c267
vhrlaptop1 <vhrlaptop1@gmail.com>
2025-01-13 17:14:37 -0500

    meow meeeeoooooww
    jwakcnkshxkhesi java;;;;;;

---
+++ src/main/java/frc/robot/climber/climbersubmarine.java
@@ -0,0 +1,19 @@
+package frc.robot.climber;
+
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+import com.revrobotics.spark.SparkLimitSwitch;
+import com.revrobotics.spark.SparkLowLevel;
+import com.revrobotics.spark.SparkBase.PersistMode;
+import com.revrobotics.spark.SparkFlex;
+import com.revrobotics.spark.config.SparkFlexConfig;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.revrobotics.spark.config.SparkMaxConfig;
+import com.revrobotics.spark.SparkBase.ResetMode;
+
+
+public class climbersubmarine extends TimedRobot{
+    
+}
