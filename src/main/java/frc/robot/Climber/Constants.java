f8262189e5d8663073c96652dab74af50dde0992
vhrlaptop6 <122494298+vhrlaptop6@users.noreply.github.com>
2025-01-13 17:07:20 -0500

    meoow meow

--- src/main/java/frc/robot/Climber/Constants.java
+++ src/main/java/frc/robot/Climber/Constants.java
@@ -1,5 +1,11 @@
 package frc.robot.Climber;
 
+import java.lang.Math;
+
 public class Constants {
-    
+    public static double LiftSpeedInitial = 10;
+    public static double LiftSpeed = 12;    
+    public static double LiftSpeedEnd = 0;    
+    public static double Rotation = Math.PI/2;    
+
 }
