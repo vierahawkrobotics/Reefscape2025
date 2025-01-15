3a45ef8d58a2d5305c4ab8b601a57cbc96b5b799
vhrlaptop7 <97807804+vhrlaptop7@users.noreply.github.com>
2025-01-15 17:50:04 -0500

    Convention Updates
    Moved around a lot of code to different files within the Drivetrain folder, still a work in progress- pushing to save

--- src/main/java/frc/robot/Drivetrain/Drivetrain.java
+++ src/main/java/frc/robot/Drivetrain/Drivetrain.java
@@ -1,23 +1,25 @@
 package frc.robot.Drivetrain;
 
-import java.util.Map;
-
 import com.revrobotics.spark.SparkBase.ControlType;
 
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
 import edu.wpi.first.math.kinematics.SwerveModuleState;
-import edu.wpi.first.networktables.GenericEntry;
 import edu.wpi.first.wpilibj.XboxController;
-import edu.wpi.first.wpilibj.shuffleboard.BuiltInWidgets;
 import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Robot;
 
 public class Drivetrain extends SubsystemBase {
   //TO DO: this can be changed later for area effects etc, note it must be meters/second
   double maxSpeed = 4;
+  XboxController controller = Robot.instance.controller;
+
+  //TO DO: replace all uses of this variable with the angle from the position subsystem in future
+  public Rotation2d currentRobotAngle = new Rotation2d();
+  
   //this should ALWAYS be front left, front right, back left, and then back right
   public static SwerveDriveKinematics kinematics = new SwerveDriveKinematics(
   DrivetrainConstants.frontLeftLocation, 
@@ -36,9 +38,7 @@ public class Drivetrain extends SubsystemBase {
   public static ShuffleboardTab drivetrainTab = Shuffleboard.getTab("Drivetrain");
 
   public double rotationSensitivity;
-  public static double rotationSensitivityDefault = .9;
   
-  public GenericEntry rotSens;
 
   //double velX,velY;
   //double velR;
@@ -46,26 +46,12 @@ public class Drivetrain extends SubsystemBase {
   //double posR;
 
   
-  //Code to make sure Drivetrain only has one instance, please use getInstance to get an instance of Drivetrain
-  private static final Drivetrain drivetrain = new Drivetrain();
-
-  private Drivetrain() {
-    rotSens = Shuffleboard.getTab("Drivetrain")
-    .add("Rotation Sensitivity", rotationSensitivityDefault)
-    .withWidget(BuiltInWidgets.kNumberSlider)
-    .withProperties(Map.of("min", 0.3, "max", 2.5))
-    .getEntry();
+  public Drivetrain() {
   }
-  
-  public static Drivetrain getInstance(){return drivetrain;}
 
-  public void exampleMethodCommand() {}
-  
   @Override
   public void periodic() {
     // This method will be called once per scheduler run\
-    // rotationSensitivity isn't upating
-    rotationSensitivity = rotSens.getDouble(rotationSensitivityDefault);
 
     //switch xyState:
     //velocity:
@@ -90,34 +76,16 @@ public class Drivetrain extends SubsystemBase {
   public void simulationPeriodic() {
     // This method will be called once per scheduler run during simulation
   }
-  
-  //remove these
-  public double getControllerX(){
-    double controllerX = (controller.getLeftX() < 0) ? Math.pow(controller.getLeftX(),2)*(-1): Math.pow(controller.getLeftX(),2);
-    controllerX = (controllerX< DrivetrainConstants.inputDeadband)?controllerX=0: controllerX*maxSpeed;
-    return controllerX;
+
+  public void setDrive3d(double vx, double vy, double vr){
+    //THis will get replaced with setting the velocities that get checked and 
+    //used in the periodic function
+    DriveVelocity(vx, vy, vr);
   }
 
-  public double getControllerY(){
-    //takes the controller input for the controller's Y axis, squares it adding back the negative if the input was negative
-    double controllerY = (controller.getLeftY() < 0) ? Math.pow(controller.getLeftY(),2)*(-1): Math.pow(controller.getLeftY(),2);
-    //applies an input deadband if the input is out of the deadband scale it using the maxSpeed
-    controllerY = (controllerY< DrivetrainConstants.inputDeadband) ? controllerY=0: controllerY*maxSpeed;
-    return controllerY;
-  }
-
-  public double getControllerRot(){
-    System.out.println(rotationSensitivity);
-    double controllerRot = controller.getRightX();
-    controllerRot = (controllerRot< DrivetrainConstants.inputDeadband)?controllerRot=0: controllerRot*rotationSensitivity;
-    return controllerRot;
-  }
-
-  //make currentRotation a variable within the function not a parameter
-  //should be private
-  //rename to DriveVelocity
-  public void setDrivetrain(double desiredX, double desiredY, double desiredRot, Rotation2d currentRotation){
-    ChassisSpeeds speeds = ChassisSpeeds.fromFieldRelativeSpeeds(desiredX, desiredY, desiredRot, currentRotation);
+  private void DriveVelocity(double desiredvX, double desiredvY, double desiredRot){
+    Rotation2d currentRotation = currentRobotAngle;
+    ChassisSpeeds speeds = ChassisSpeeds.fromFieldRelativeSpeeds(desiredvX, desiredvY, desiredRot, currentRotation);
 
     SwerveModuleState[] moduleStates = Drivetrain.kinematics.toSwerveModuleStates(speeds);
     
@@ -127,7 +95,7 @@ public class Drivetrain extends SubsystemBase {
       moduleStates[i].speedMetersPerSecond *= moduleStates[i].angle.minus(currentAngle).getCos();
     }
 
-    Drivetrain.getInstance().setDesiredStates(moduleStates);
+    Robot.instance.drivetrain.setDesiredStates(moduleStates);
   }
 
   //private void DrivePosition(posX,posY)
