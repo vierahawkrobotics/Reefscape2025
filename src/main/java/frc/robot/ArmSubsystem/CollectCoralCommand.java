365f650ce61aee45c50b9a9ba9ab79039054d822
vhrlaptop4 <94511164+vhrlaptop4@users.noreply.github.com>
2025-01-22 17:48:53 -0500

    Command Updates
    GrabDropCommand, ElevatorUpDownCommand, CollectCoralCommand

--- src/main/java/frc/robot/ArmSubsystem/CollectCoralCommand.java
+++ src/main/java/frc/robot/ArmSubsystem/CollectCoralCommand.java
@@ -1,18 +1,31 @@
 package frc.robot.ArmSubsystem;
+import com.revrobotics.spark.SparkMax;
+
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.Robot;
 public class CollectCoralCommand extends Command {
     
-    public CollectCoralActionCommand() {
+    private SparkMax motor;
+
+    public CollectCoralCommand() {
         addRequirements(Robot.instance.exampleSubsystem);
     }
 
     @Override
-    public void initialize() {}
+    public void initialize() {
+        motor = new SparkMax(0, null);
+    }
+
     @Override
-    public void execute() {}
+    public void execute() {
+
+    }
+
     @Override
-    public void end(boolean interrupted) {}
+    public void end(boolean interrupted) {
+
+    }
+
     @Override
     public boolean isFinished() {
         return false;
--- src/main/java/frc/robot/ArmSubsystem/ElevatorUpDownCommand.java
+++ src/main/java/frc/robot/ArmSubsystem/ElevatorUpDownCommand.java
@@ -1,24 +1,45 @@
+
 package frc.robot.ArmSubsystem;
+import edu.wpi.first.wpilibj.Solenoid;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.Robot;
 
-public class ElevatorUpDownCommand extends Command {
-    public ElevatorUpDownCommand() {
+import com.ctre.phoenix6.signals.ForwardLimitSourceValue;
+import com.revrobotics.spark.SparkFlex;
+import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+
+public class ElevatorUpDownActionCommand extends Command {
+
+    private SparkMax motor;
+
+    public ElevatorUpDownActionCommand() {
         addRequirements(Robot.instance.exampleSubsystem);
     }
 
     @Override
     public void initialize() {
-        if (/*Check Up Button Pressed */) {
-            // Increment Arm Up
-        } else if (/*Check Down Button Pressed */) {
-            // Increment Arm Down
+        motor = new SparkMax(0, null)
+    }
+
+    @Override
+    public void execute() {
+        if (autoCommand != null) {
+            motor.set(1.0);
+        }
+        else if (autoCommand != null) {
+            motor.set(-1.0);
+        }
+        else {
+            motor.set(0);
         }
     }
+
     @Override
-    public void execute() {}
-    @Override
-    public void end(boolean interrupted) {}
+    public void end(boolean interrupted) {
+        motor.set(0);
+    }
+    
     @Override
     public boolean isFinished() {
         return false;
