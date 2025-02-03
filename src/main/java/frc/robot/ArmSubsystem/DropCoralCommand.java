9aef4860643cd9e46a7b46b36736228b11042a6f
vhrlaptop3 <94510927+vhrlaptop3@users.noreply.github.com>
2025-02-03 17:44:45 -0500

    Mechanical Being Annoying 2.1
    setup switch case in drop coral

--- src/main/java/frc/robot/ArmSubsystem/DropCoralCommand.java
+++ src/main/java/frc/robot/ArmSubsystem/DropCoralCommand.java
@@ -6,6 +6,7 @@ import frc.robot.Robot;
 
 public class DropCoralCommand extends Command {
     public static double startTime;
+    private DropState state;
     public DropCoralCommand() {
         addRequirements(Robot.instance.exampleSubsystem);
     }
@@ -14,8 +15,41 @@ public class DropCoralCommand extends Command {
     public void initialize() {}
     @Override
     public void execute() {
-        ArmSubsystem.setIntakeState(ArmSubsystem.IntakeState.Collect);
+        switch(state) {
+            case SetupInit:
+                Robot.instance.armSubsystem.setPosition(this.hState);
+                //set robot position
+                Robot.instance.drivetrainSubsystem.setPosition(TriggerEffect.getAlgeaPose(this.hState));
+                state = DropState.SetupPeriodic;
+                break;
+            case SetupPeriodic:
+                if(drivetrain.isAtTargetPose() && Robot.instance.armSubsystem.atTargetHeight())
+                    state = DropState.ExtendInit;
+                break;
+            case ExtendInit:
+                //move robot forward
+                state = DropState.ExtendPeriodic;
+                break;
+            case ExtendPeriodic:
+                if(drivetrain.isAtTargetPose())
+                    state = DropState.EjectInit;
+                break;
+            case DropInit:
+                Robot.instance.armSubsystem.ejectAlgae();
+                state = DropState.EjectPeriodic;
+                break;
+            case DropPeriodic:
+                // (move robot back a foot) Robot.instance.drivetrainSubsystem.
+                //arm.ejectAlgae()
+                //rotate eject wheels and move arm up
+                //robot move back a foot
+                if(IsFinished()){
+                    state = DropState.End;
+                }
+                break;
+        }
         startTime = Timer.getFPGATimestamp();
+        ArmSubsystem.setIntakeState(ArmSubsystem.IntakeState.Drop);
     }
     @Override
     public void end(boolean interrupted) {}
@@ -26,5 +60,15 @@ public class DropCoralCommand extends Command {
         } else {
             return false;
         }
+    }
+
+    enum DropState {
+        SetupInit,
+        SetupPeriodic,
+        ExtendInit,
+        ExtendPeriodic,
+        DropInit,
+        DropPeriodic,
+        End
     }
 }
