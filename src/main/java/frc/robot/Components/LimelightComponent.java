4cef2d9f96e752bc4401802b639b2f53664737f2
vhrlaptop1 <vhrlaptop1@gmail.com>
2025-02-10 17:39:06 -0500

    Update LimelightComponent.java
    Fixed the messed up merge and added the Blue or Red Alliance differentiation code.

--- src/main/java/frc/robot/Components/LimelightComponent.java
+++ src/main/java/frc/robot/Components/LimelightComponent.java
@@ -1,15 +1,31 @@
 package frc.robot.Components;
 
+import frc.robot.LimelightHelpers;
+
+import java.util.Optional;
+
 import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.DriverStation.Alliance;
 
 public class LimelightComponent {
     public static Pose2d calcAprilTag() {
-        boolean hasTarget = false; // Fix to status
-        
+        LimelightHelpers.PoseEstimate limelightMeasurement;
+        boolean hasTarget = LimelightHelpers.getTV("");
+
         if(hasTarget){
-            return new Pose2d(); // Fix to pose2d based on april tag positioning
-        }else{
-            return null; // No valid target, return null
+            Optional<Alliance> ally = DriverStation.getAlliance();
+            if (ally.isPresent()) {
+                if (ally.get() == Alliance.Red) {
+                    limelightMeasurement = LimelightHelpers.getBotPoseEstimate_wpiRed("");
+                } else if(ally.get() == Alliance.Blue){
+                    limelightMeasurement = LimelightHelpers.getBotPoseEstimate_wpiBlue("");
+                } else {
+                    limelightMeasurement = null;
+                }
+                return(limelightMeasurement.pose);
+            }
         }
+        return(null);
     }
 }
