7509664e5fbf7a392c86171441b7880dd6d7f18e
vhrlaptop10 <vhrlaptop10@gmail.com>
2025-02-12 17:44:10 -0500

    Implemented some arm subsystem tests

--- src/main/java/frc/robot/Match/RobotState.java
+++ src/main/java/frc/robot/Match/RobotState.java
@@ -3,12 +3,25 @@ package frc.robot.Match;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 import frc.robot.Robot;
+import frc.robot.ArmSubsystem.CollectCoralCommand;
+import frc.robot.ArmSubsystem.DropCoralCommand;
+import frc.robot.ArmSubsystem.ElevatorUpDownCommand;
+import frc.robot.ArmSubsystem.RemoveAlgaeCommand;
 
 public class RobotState {
     public static XboxController controller;
     public static void Initialize() {
         controller = new XboxController(0);
+        //Defines button A which calls function null
         new JoystickButton(controller, XboxController.Button.kA.value).onTrue(null);
+        //defines button B which calls function CollectCoralCommand
+        new JoystickButton(controller, XboxController.Button.kB.value).onTrue(new CollectCoralCommand());
+        //defines button X which calls function DropCoralCommand
+        new JoystickButton(controller, XboxController.Button.kX.value).onTrue(new DropCoralCommand());
+        //defines button Y which calls function RemoveAlgaeCommand
+        new JoystickButton(controller, XboxController.Button.kY.value).onTrue(new RemoveAlgaeCommand());
+        //defines button LB which calls function ElevatorUpDownCommand
+        new JoystickButton(controller, XboxController.Button.kLeftBumper.value).onTrue(new ElevatorUpDownCommand());
     }
     public static void Periodic() {}
 }
