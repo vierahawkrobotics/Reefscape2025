684756d32a780d24c34a97f70e51f6a13ad9e6ea
vhrlaptop3 <94510927+vhrlaptop3@users.noreply.github.com>
2025-02-14 15:58:32 -0500

    CANdle Complete?

--- src/main/java/frc/robot/Components/CANdleConstants.java
+++ src/main/java/frc/robot/Components/CANdleConstants.java
@@ -8,7 +8,9 @@ public class CANdleConstants {
         AlgaeRemoval,
         RobotMoving;
     }
-    
+
     public final static int CANdleID = -1;
     public final static int numLED = -1;
+    public final static double offScalar = 0;
+    public final static double onScalar = 0.1;
 }
--- src/main/java/frc/robot/Components/CANdleController.java
+++ src/main/java/frc/robot/Components/CANdleController.java
@@ -2,6 +2,11 @@ package frc.robot.Components;
 
 import com.ctre.phoenix.led.Animation;
 import com.ctre.phoenix.led.CANdle;
+import com.ctre.phoenix.led.ColorFlowAnimation;
+import com.ctre.phoenix.led.FireAnimation;
+import com.ctre.phoenix.led.LarsonAnimation;
+import com.ctre.phoenix.led.RainbowAnimation;
+
 import frc.robot.Components.CANdleConstants.RobotStates;
 
 public class CANdleController {
@@ -23,21 +28,28 @@ public class CANdleController {
     public void setAnimation(RobotStates animation) {
         switch (animation) {
             case Intaking:
-
+                animate = new ColorFlowAnimation(128, 20, 70, 0, 0.7, CANdleConstants.numLED, ColorFlowAnimation.Direction.Forward);
                 break;
             case Dropping:
+                animate = new FireAnimation(0.5, 0.7, CANdleConstants.numLED, 0.7, 0.5);
                 break;
             case AlgaeRemoval:
+                animate = new LarsonAnimation(0, 255, 46, 0, 1, CANdleConstants.numLED, LarsonAnimation.BounceMode.Front, 3);
                 break;
             case RobotMoving:
-                break;
+                animate = new RainbowAnimation(1, 0.1, CANdleConstants.numLED);
             case Idle:
             default:
-                break;
+                animate = null;
         }
     }
 
     public void Periodic() {
-        candle.animate(animate);
+        if (animate == null) {
+            candle.configBrightnessScalar(CANdleConstants.offScalar);
+        } else {
+            candle.configBrightnessScalar(CANdleConstants.onScalar);
+            candle.animate(animate);
+        }
     }
 }
