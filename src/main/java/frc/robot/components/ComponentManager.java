30bf95abc9e9a22821382211a02456dde910696f
vhrlaptop3 <94510927+vhrlaptop3@users.noreply.github.com>
2025-02-14 16:06:54 -0500

    Manager Setup

--- src/main/java/frc/robot/components/ComponentManager.java
+++ src/main/java/frc/robot/components/ComponentManager.java
@@ -1,10 +1,12 @@
 package frc.robot.Components;
 
 public class ComponentManager {
+    static CANdleController CANdle;
     public static void Initialize() {
         ComponentExample.Initialize();
+        CANdle = new CANdleController();
     }
     public static void Periodic() {
-
+        CANdle.Periodic();
     }
 }
