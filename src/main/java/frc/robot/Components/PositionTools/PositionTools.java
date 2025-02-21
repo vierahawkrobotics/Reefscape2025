d3ff47145b8ad0b875921dba0bda8ec835999729
vhrlaptop8 <97808146+vhrlaptop8@users.noreply.github.com>
2025-02-21 16:52:19 -0500

    Fixed positioning and bugs
    Removed ArmSubsytem, added shuffleboard to positioning, restructured PositionComponent, fixed misc. bugs.

--- src/main/java/frc/robot/Components/PositionTools/PositionTools.java
+++ src/main/java/frc/robot/Components/PositionTools/PositionTools.java
@@ -28,10 +28,10 @@ public class PositionTools {
         new Pose2d(0,PositionConstants.ScoringLocations.backOffset,Rotation2d.fromDegrees(isRotated ? 90: 0)));
     }
 
-    public static Pose2d closestScorePose(boolean isRotated){
+    public static Pose2d closestScorePose(boolean isRotated, double limitOffset){
         if(isRotated){
             return getPoseTranslated(trueclosestScore(),
-            new Pose2d(Robot.instance.armSubsystem.isLimitSwitchPressed(), 0, Rotation2d.fromDegrees(0)));
+            new Pose2d(limitOffset, 0, Rotation2d.fromDegrees(0)));
         } else {
             return trueclosestScore().rotateBy(Rotation2d.fromDegrees(90));
         }
--- src/main/java/frc/robot/components/ComponentManager.java
+++ src/main/java/frc/robot/components/ComponentManager.java
@@ -1,13 +1,26 @@
 package frc.robot.Components;
 
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
 import frc.robot.Components.AreaEffects.AreaEffectsHandler;
 
 public class ComponentManager {
+    private static ShuffleboardTab posTab;
     public static void Initialize() {
+        PositionComponent.initialize(new Pose2d());
         AreaEffectsHandler.initialize();
         HighwaySystem.initialize();
+
+        //Shuffleboard stuff
+        posTab = Shuffleboard.getTab("Position Data");
+        posTab.addDouble("Drive Estimated X", ()->{return PositionComponent.getRobotPose().getX();});
+        posTab.addDouble("Drive Estimated Y", ()->{return PositionComponent.getRobotPose().getY();});
+        posTab.addDouble("Drive Estimated Rotation (degrees)", ()->{return PositionComponent.getRobotPose().getRotation().getDegrees();});
+        posTab.addDouble("Gyroscope Estimated Rotation (degrees)", ()->{return PositionComponent.getGyroRotation();});
+        
     }
     public static void Periodic() {
-        //AreaEffectsHandler.periodic();
+        
     }
 }
