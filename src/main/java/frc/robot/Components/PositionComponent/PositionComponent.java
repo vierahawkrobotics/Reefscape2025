332cfc3806b2335910ec4e11d40fcada12bd221c
vhrlaptop8 <97808146+vhrlaptop8@users.noreply.github.com>
2025-02-21 19:52:10 -0500

    Fixed potentially buggy velocity

--- src/main/java/frc/robot/Components/PositionComponent/PositionComponent.java
+++ src/main/java/frc/robot/Components/PositionComponent/PositionComponent.java
@@ -25,8 +25,8 @@ public class PositionComponent {
     private static SwerveDrivePoseEstimator poseEstimator;
     private static SwerveModulePosition[] wheelPositions;
     private static AHRS gryoObject;
-    private static Pose2d lastPose;
-    private static long lastTimestamp;
+    private static Pose2d[] lastPose = new Pose2d[2];
+    private static long[] lastTimestamp = new long[2];
     private static PositionComponent instance;
 
     private PositionComponent(Pose2d initialPose) {
@@ -34,6 +34,12 @@ public class PositionComponent {
         gryoObject.reset();
         Rotation2d initialRot = Rotation2d.fromDegrees(gryoObject.getAngle());
         poseEstimator = new SwerveDrivePoseEstimator(Drivetrain.kinematics, initialRot, Drivetrain.getSwerveModulePositions(), initialPose); // Fix kinematics and modulePositions parameter
+        lastPose[0] = poseEstimator.getEstimatedPosition();
+        lastPose[1] = poseEstimator.getEstimatedPosition();
+
+        lastTimestamp[0] = edu.wpi.first.wpilibj.RobotController.getFPGATime();
+        lastTimestamp[1] = edu.wpi.first.wpilibj.RobotController.getFPGATime() - 20; 
+
     }
     public static PositionComponent initialize(Pose2d initialPose){
         instance  = new PositionComponent(initialPose);
@@ -56,7 +62,7 @@ public class PositionComponent {
     public static ChassisSpeeds getChassisSpeeds(velType velMethod){
         switch(velMethod){
             case kOdometry:
-                Transform2d delta = getRobotPose().minus(lastPose).div((double)(lastTimestamp - edu.wpi.first.wpilibj.RobotController.getFPGATime()));
+                Transform2d delta = lastPose[0].minus(lastPose[1]).div((double)((lastTimestamp[0] - lastTimestamp[1])/1000000));
                 return new ChassisSpeeds(delta.getX(), delta.getY(), delta.getRotation().getRadians());
             case kGyroscope:
                 return new ChassisSpeeds(gryoObject.getVelocityX(), gryoObject.getVelocityY(), gryoObject.getVelocityZ());
@@ -81,8 +87,10 @@ public class PositionComponent {
 
     public static void periodic(){
         poseEstimator.update(Rotation2d.fromDegrees(gryoObject.getAngle()), Drivetrain.getSwerveModulePositions());
-        lastTimestamp = edu.wpi.first.wpilibj.RobotController.getFPGATime();
-        lastPose = poseEstimator.getEstimatedPosition();
+        lastTimestamp[1] = lastTimestamp[0];
+        lastTimestamp[0] = edu.wpi.first.wpilibj.RobotController.getFPGATime();
+        lastPose[1] = lastPose[1];
+        lastPose[0] = poseEstimator.getEstimatedPosition();
 
 
         // if(LimelightComponent.calcAprilTag() != null){
