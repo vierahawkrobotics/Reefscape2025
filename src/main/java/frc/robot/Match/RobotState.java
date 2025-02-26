3d121566f6da7d901d1c09a02029cd7e50f1b640
vhrlaptop3 <94510927+vhrlaptop3@users.noreply.github.com>
2025-02-26 18:02:52 -0500

    Some Controls Progress

--- src/main/java/frc/robot/Match/RobotState.java
+++ src/main/java/frc/robot/Match/RobotState.java
@@ -1,37 +1,33 @@
 package frc.robot.Match;
 
 import edu.wpi.first.wpilibj.XboxController;
-import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 import frc.robot.Robot;
 
-import frc.robot.ArmSubsystem.CollectCoralCommand;
-import frc.robot.ArmSubsystem.DropCoralCommand;
-import frc.robot.ArmSubsystem.ElevatorUpDownCommand;
-import frc.robot.ArmSubsystem.RemoveAlgaeCommand;
 import frc.robot.Drivetrain.Drive3D;
 
 public class RobotState {
-    public static XboxController controller;
+    public static XboxController controller1;
+    public static XboxController controller2;
     public static void Initialize() {
-        controller = new XboxController(0);
-        // Defines button B which calls function CollectCoralCommand
-        new JoystickButton(controller, XboxController.Button.kB.value).onTrue(new CollectCoralCommand());
-        // Defines button X which calls function DropCoralCommand
-        new JoystickButton(controller, XboxController.Button.kX.value).onTrue(new DropCoralCommand());
-        // Defines button left bumper which calls function RemoveAlgaeCommand
-        new JoystickButton(controller, XboxController.Button.kLeftBumper.value).onTrue(new RemoveAlgaeCommand());
-        // Defines button Y which moves elevator up
-        new JoystickButton(controller, XboxController.Button.kY.value).onTrue(new ElevatorUpDownCommand(true,false));
-        // Defines button A which moves elevator up
-        new JoystickButton(controller, XboxController.Button.kA.value).onTrue(new ElevatorUpDownCommand(false,true));
+        controller1 = new XboxController(0);
+        controller2 = new XboxController(1);
 
+        // Controller 1
+        //  Left Joystick - Movement, Right Joystick - Rotation
         Robot.instance.drivetrain.setDefaultCommand(new Drive3D(() -> {
-            return controller.getLeftY();
+            return controller1.getLeftY();
         }, () -> {
-            return controller.getLeftX();
+            return controller1.getLeftX();
         }, () -> {
-            return controller.getRightX();
+            return controller1.getRightX();
         }));
+
+        // Controller 2
+        //  Container
+
+        //  Elevator
+
+        //  Climber
     }
     public static void Periodic() {}
 }
