29a294bdea18c70aeddb5f185e044ee79636c26c
vhrlaptop8 <97808146+vhrlaptop8@users.noreply.github.com>
2025-03-01 15:23:53 -0500

    Added Limelight Pose Logging

--- src/main/java/frc/robot/Components/LimelightComponent.java
+++ src/main/java/frc/robot/Components/LimelightComponent.java
@@ -9,8 +9,14 @@ import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.DriverStation.Alliance;
 
 public class LimelightComponent {
+    private static Pose2d lPos = new Pose2d();
+    public static Pose2d getLastAprilTag(){
+        calcAprilTag();
+        return lPos;
+
+    }
     public static Pose2d calcAprilTag() {
-        LimelightHelpers.PoseEstimate limelightMeasurement;
+        LimelightHelpers.PoseEstimate limelightMeasurement = null;
         boolean hasTarget = LimelightHelpers.getTV("");
 
         if(hasTarget){
@@ -20,12 +26,15 @@ public class LimelightComponent {
                     limelightMeasurement = LimelightHelpers.getBotPoseEstimate_wpiRed("");
                 } else if(ally.get() == Alliance.Blue){
                     limelightMeasurement = LimelightHelpers.getBotPoseEstimate_wpiBlue("");
-                } else {
-                    limelightMeasurement = null;
                 }
-                return(limelightMeasurement.pose);
             }
         }
-        return(null);
+
+        if(limelightMeasurement != null){
+            lPos = limelightMeasurement.pose;
+            return limelightMeasurement.pose;
+        } else {
+            return null;
+        }
     }
 }
