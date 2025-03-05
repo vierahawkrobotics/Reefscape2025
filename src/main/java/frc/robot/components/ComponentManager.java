b434c2a3d6f9d82496cf6241ffb4fc914179fce8
vhrlaptop10 <vhrlaptop10@gmail.com>
2025-03-05 16:33:17 -0500

    add system for switchable low current

--- src/main/java/frc/robot/components/ComponentManager.java
+++ src/main/java/frc/robot/components/ComponentManager.java
@@ -13,6 +13,7 @@ public class ComponentManager {
         PositionComponent.initialize(new Pose2d());
         AreaEffectsHandler.initialize();
         HighwaySystem.initialize();
+        PDHManager.Initialize();
 
         //Shuffleboard stuff
         posTab = Shuffleboard.getTab("Position Data");
@@ -40,5 +41,6 @@ public class ComponentManager {
     public static void Periodic() {
         PositionComponent.periodic();
         AreaEffectsHandler.periodic();
+        PDHManager.Periodic();
     }
 }
