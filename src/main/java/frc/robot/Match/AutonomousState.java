cb39a74627db1fa318ac7c3d93cd572b84491cfd
vhrlaptop4 <94511164+vhrlaptop4@users.noreply.github.com>
2025-03-07 17:03:47 -0500

    Revert "Start Pathplanner implementation"
    This reverts commit c11c81f76c87bd33857cc270f6f88d98a0dff8c5.

--- src/main/java/frc/robot/Match/AutonomousState.java
+++ src/main/java/frc/robot/Match/AutonomousState.java
@@ -1,21 +1,12 @@
 package frc.robot.Match;
 
-import com.pathplanner.lib.auto.AutoBuilder;
-import com.pathplanner.lib.commands.PathPlannerAuto;
-
-import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj2.command.Command;
 
 public class AutonomousState {
-      private final SendableChooser<Command> autoChooser;
-
-
     public static Command getAutoCommand() {
-        return new PathPlannerAuto("Auto Name");
+        return null;
     }
-    public static void initialize() {
-        autoChooser = AutoBuilder.buildAutoChooser();
-    }
+    public static void initialize() {}
     public static void periodic() {}
     public static void exit() {}
 }
